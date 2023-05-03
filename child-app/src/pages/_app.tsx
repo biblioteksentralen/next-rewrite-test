401b3ebba6527330b683b1b2dcb94231f99ade5a
Daniel Winsvold <danielwinsvold@gmail.com>
2023-05-03 11:19:19 +0200

    log devinfo i child app as well

--- child-app/src/pages/_app.tsx
+++ child-app/src/pages/_app.tsx
@@ -1,5 +1,7 @@
+import { useLogDevInfo } from '@/logDevInfo'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
+  useLogDevInfo();
   return <Component {...pageProps} />
 }
