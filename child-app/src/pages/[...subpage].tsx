1c529e9e681a908782f306d9ffdfe3dcfa375757
Daniel Winsvold <danielwinsvold@gmail.com>
2023-05-03 11:29:02 +0200

    getStaticProps i child-app

--- child-app/src/pages/[...subpage].tsx
+++ child-app/src/pages/[...subpage].tsx
@@ -1,9 +1,21 @@
+import { GetStaticPaths, GetStaticProps } from "next";
 import { useRouter } from "next/router";
 
-const LogParams = () => {
+export const getStaticPaths: GetStaticPaths = async () => ({ paths: [], fallback: "blocking" });
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const params = ctx.params;
+
+  return {
+    props: { params },
+    revalidate: 15,
+  };
+};
+
+const LogParams = (props) => {
   const router = useRouter();
 
-  console.log(router);
+  console.log({router, props});
 
   return (
     <div>
