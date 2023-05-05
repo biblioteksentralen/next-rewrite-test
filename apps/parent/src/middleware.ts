506b87755aa242444f5a1887a3440ff3b103f507
Daniel Winsvold <danielwinsvold@gmail.com>
2023-05-05 15:05:22 +0200

    Revert "temp test"
    This reverts commit 350c20d59b78f1a8df7c1af76834d9294bb2f474.

--- apps/parent/src/middleware.ts
+++ apps/parent/src/middleware.ts
@@ -9,24 +9,29 @@ export const config = {
 export default async function middleware(req: NextRequest) {
   const { pathname, locale: originalLocale } = req.nextUrl;
 
+  const currentDomain = req.headers.get("host") || "";
+
+  const newHeaders = new Headers(req.headers);
+  newHeaders.set("current-domain", currentDomain);
+
   if (pathname.startsWith("/child1")) {
     const url = isDevelopment
       ? `http://localhost:3001${pathname}`
       : `https://next-rewrite-test-child-1.vercel.app${pathname}`;
-    return NextResponse.rewrite(url);
+    return NextResponse.rewrite(url, { request: { headers: newHeaders } });
   }
 
   if (pathname.startsWith("/child2")) {
     const url = isDevelopment
       ? `http://localhost:3002${pathname}`
       : `https://next-rewrite-test-child-2.vercel.app${pathname}`;
-    return NextResponse.rewrite(url);
+    return NextResponse.rewrite(url, { request: { headers: newHeaders } });
   }
 
   const url = req.nextUrl.clone();
-  url.pathname = `/sites/${"currentDomain"}${pathname}`;
+  url.pathname = `/sites/${currentDomain}${pathname}`;
   console.log(
-    `🏛 Domain: ${"currentDomain"} 🚚 Rewrite: ${req.url} → ${url.toString()}`
+    `🏛 Domain: ${currentDomain} 🚚 Rewrite: ${req.url} → ${url.toString()}`
   );
   return NextResponse.rewrite(url);
 }
