4e79f69ffdb59ce37ecd974d4b1bbc1db3c05ac4
Daniel Winsvold <danielwinsvold@gmail.com>
2023-05-15 12:15:16 +0200

    prøver   experimental: {     externalMiddlewareRewritesResolve: true   }

--- apps/child1/next.config.js
+++ apps/child1/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/child1"
+  basePath: "/child1",
+  experimental: {
+    externalMiddlewareRewritesResolve: true
+  }
 }
 
 module.exports = nextConfig
